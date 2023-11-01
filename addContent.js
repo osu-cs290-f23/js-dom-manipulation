cdcf13d6e781f84dde0eb48b55d64115118b1ac8
Rob Hess <hessro@oregonstate.edu>
2023-11-01 15:26:08 -0700

    Add code from 11/1 lecture.

--- addContent.js
+++ addContent.js
@@ -6,3 +6,37 @@ var firstPhotoCard = photoCardContainer.children[0]
 var userSuppliedValue = "<img src=x onerror='alert(\"Uh oh!\")' />"
 // firstPhotoCard.innerHTML = "<p>" + userSuppliedValue + "</p>"
 firstPhotoCard.textContent = userSuppliedValue
+
+function insertPhotoCard(url, caption) {
+    var photoCardSection = document.createElement("section")
+    photoCardSection.classList.add("photo-card", "another-class", "a-third-class")
+    photoCardSection.classList.remove("another-class")
+    photoCardSection.classList.toggle("another-class")
+
+    var imgContainerDiv = document.createElement("div")
+    imgContainerDiv.classList.add("img-container")
+    photoCardSection.appendChild(imgContainerDiv)
+
+    var personPhotoImg = document.createElement("img")
+    personPhotoImg.classList.add("person-photo-img")
+    personPhotoImg.src = url
+    imgContainerDiv.appendChild(personPhotoImg)
+
+    var captionDiv = document.createElement("div")
+    captionDiv.classList.add("caption")
+    photoCardSection.appendChild(captionDiv)
+    captionDiv.textContent = caption
+
+    console.log("== photoCardSection:", photoCardSection)
+    photoCardContainer.appendChild(photoCardSection)
+}
+
+insertPhotoCard(
+    "http://placekitten.com/480/480",
+    "Luke as a kitty"
+)
+
+insertPhotoCard(
+    "http://placekitten.com/480/480?image=2",
+    "Luke as a kitty #2"
+)
